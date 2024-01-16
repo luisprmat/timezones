3d36cc0973583e03f75699f62228edbc2da0d362
Luis Parrado <luisprmat@gmail.com>
2024-01-15 22:06:11 -0500

    Adding Notification Relationship to User and Booking Models

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Booking extends Model
 {
@@ -16,5 +17,10 @@ class Booking extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function scheduledNotifications(): MorphMany
+    {
+        return $this->morphMany(ScheduledNotification::class, 'notifiable');
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Booking::class);
     }
+
+    public function scheduledNotifications(): HasMany
+    {
+        return $this->hasMany(ScheduledNotification::class);
+    }
 }
